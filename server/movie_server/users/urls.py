64282e00e96b7a1a184eb15ae42f5b6905cf7e5e
mrdivinemaniac <divinemaniac@gmail.com>
2018-07-12 21:07:21 +0545

    Create api to get currently logged in user's data

--- server/movie_server/users/urls.py
+++ server/movie_server/users/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, CurrentUserView
 
 
 router = routers.DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+    url('me/', CurrentUserView.as_view()),
     url(r'^', include(router.urls))
 ]
--- server/movie_server/users/views.py
+++ server/movie_server/users/views.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
 from .serializers import UserSerializer
+from django.http import Http404
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAdminUser,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class CurrentUserView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    def get_object(self, id):
+        try:
+            return User.objects.get(id=id)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, format=None):
+        user = self.request.user
+        thisUser = UserSerializer(self.get_object(user.id))
+        return Response(thisUser.data)
