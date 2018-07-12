c15eadb14c773384d7a0bd7af3ff07ab97c2ab73
mrdivinemaniac <divinemaniac@gmail.com>
2018-07-12 21:42:08 +0545

    Add token authentication to settings

--- server/movie_server/movie_server/settings.py
+++ server/movie_server/movie_server/settings.py
@@ -134,5 +134,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
