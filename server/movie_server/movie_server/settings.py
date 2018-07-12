ee5a33ccb8b284c79b6caf6ed5f3e984677cfe52
mrdivinemaniac <divinemaniac@gmail.com>
2018-07-12 18:22:48 +0545

    Add token login support and cors support

--- server/movie_server/movie_server/settings.py
+++ server/movie_server/movie_server/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 ROOT_URLCONF = 'movie_server.urls'
 
--- server/movie_server/movie_server/urls.py
+++ server/movie_server/movie_server/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
@@ -22,5 +23,6 @@ urlpatterns = [
     path('api/genre/', include('genre.urls')),
     path('api/movies/', include('movies.urls')),
     path('api/auth/', include('rest_framework.urls')),
+    path('api/auth/token', obtain_auth_token),
     path('admin/', admin.site.urls),
 ]
