e9e6b101bd3bbe047553d0606785cd314521c6d0
mrdivinemaniac <divinemaniac@gmail.com>
2018-08-01 10:01:41 +0545

    Display genre name when fetching movies

--- server/movie_server/movies/serializers.py
+++ server/movie_server/movies/serializers.py
@@ -3,6 +3,7 @@ from .models import Movies
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(read_only=True, slug_field='name')
     class Meta:
         model = Movies
         fields = ('id', 'title', 'description', 'release_date', 'genre', 'created_at', 'updated_at')
