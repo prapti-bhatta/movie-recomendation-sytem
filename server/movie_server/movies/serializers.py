984d5c4b229336b4f73cfe198769efdb82acc64c
mrdivinemaniac <divinemaniac@gmail.com>
2018-08-01 19:09:29 +0545

    Read movie genre as genre_name

--- server/movie_server/movies/serializers.py
+++ server/movie_server/movies/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Movies
+from .models import Movies, MovieReviews
+from genre.models import Genre
+from users.serializers import UserSerializer
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    genre = serializers.SlugRelatedField(read_only=True, slug_field='name')
+    genre_name = serializers.StringRelatedField(source='genre', read_only=True)
+    genre = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Genre.objects.all())
+
     class Meta:
         model = Movies
-        fields = ('id', 'title', 'description', 'release_date', 'genre', 'created_at', 'updated_at')
+        fields = ('id', 'title', 'description', 'release_date', 'genre', 'genre_name', 'created_at', 'updated_at')
+
+
+class MovieReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = MovieReviews
+        fields = ('id', 'comment', 'rating', 'movie', 'user', 'created_at')
