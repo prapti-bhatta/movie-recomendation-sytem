10a6cdcce170790c22dbb3d9cb0031bc0a43f111
mrdivinemaniac <divinemaniac@gmail.com>
2018-08-02 18:14:16 +0545

    update recommendation script

---
+++ server/learning/db_operations.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+db_host = 'localhost'
+db_name = 'movie_recommendation'
+db_user = 'postgres'
+db_password = 'password'
+
+
+def connect_db():
+    conn = psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_password)
+    print('Connected to database...')
+    return conn
+
+
+def insert_recommendations (conn, recommendations):
+    print('Inserting new recommendations')
+    cur = conn.cursor()
+    args_str = b','.join(cur.mogrify("(%s,%s,%s)", x) for x in recommendations)
+    cur.execute(b"INSERT INTO recommendations_bysimilarusers (user_id, movie_id, rating) VALUES " + args_str)
+    cur.close()
+
+
+def clear_recommendations (conn):
+    print('Clearing existing recommendations...')
+    cur = conn.cursor()
+    cur.execute('DELETE FROM recommendations_bysimilarusers')
+    cur.close()
+
+
+def get_reviews (conn):
+    print('Fetching all ratings from database...')
+    cur = conn.cursor()
+    cur.execute('select user_id, json_object_agg(movie_id, rating) from movies_moviereviews GROUP BY user_id')
+    records = cur.fetchall()
+    cur.close()
+    return records
--- server/learning/generate_recommendations.py
+++ server/learning/generate_recommendations.py
@@ -1,36 +1,30 @@
+#References:
 #https://medium.com/ai-society/a-concise-recommender-systems-tutorial-fa40d5a9c0fa
 #https://cambridgespark.com/content/tutorials/implementing-your-own-recommender-systems-in-Python/index.html
 #https://hackernoon.com/introduction-to-recommender-system-part-1-collaborative-filtering-singular-value-decomposition-44c9659c5e75
-import json
-import psycopg2
+
 from pearson_similarity import similarity
 from recommend import recommend
+from db_operations import connect_db, get_reviews, insert_recommendations, clear_recommendations
 
-db_host = 'localhost'
-db_name = 'movie_recommendation'
-db_user = 'postgres'
-db_password = 'password'
+conn = connect_db()
 
-conn = psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_password)
-print('Connected to database...')
-
-print('Fetching all ratings from database...')
-cur = conn.cursor()
-
-cur.execute('select user_id, json_object_agg(movie_id, rating) from movies_moviereviews GROUP BY user_id')
-
-records = cur.fetchall()
+records = get_reviews(conn)
 
 num_users = len(records)
+
+to_insert = []
 
 for i in range(num_users):
     user_id = records[i][0]
     recommendations = recommend(records, i, 10, similarity)
-    sorted_by_rating = sorted(recommendations.items(), key=lambda kv: kv[1], reverse=True)
-    items_only = [i[0] for i in sorted_by_rating]
-    print(items_only)
+    as_tuples = [(user_id, int(item), recommendations[item]) for item in recommendations]
+    to_insert.extend(as_tuples)
 
 print('Done fetching reviews!')
 
-cur.close()
+clear_recommendations(conn)
+insert_recommendations(conn, to_insert)
+
+conn.commit()
 conn.close()
